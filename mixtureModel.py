4e12789eaddce6c0807fdf7c113735d8e2d90012
caguirre5 <107177715+caguirre5@users.noreply.github.com>
2023-03-30 23:13:35 -0600

    Algorithm done

--- mixtureModel.py
+++ mixtureModel.py
@@ -1,104 +1,110 @@
-'''
-Created with love by Sigmoid
-
-@Author - Păpăluță Vasile - vpapaluta06@gmail.com
-'''
-
-# Importing the libraries
 import numpy as np
 import pandas as pd
+
+
 class GMM:
     '''
-        This class is the implementation of the Gaussian Mixture Models 
-        inspired by sci-kit learn implementation.
+        Esta clase implementa el modelo Gausiano Mixture Models
+        basado en la implementación de la libería sci-kit learn.
     '''
-    def __init__(self, n_components, max_iter = 20, comp_names=None):
+
+    def __init__(self, n_components, max_iter=20, comp_names=None):
         '''
-            This functions initializes the model by seting the following paramenters:
+            Esta función inicializa el modelo configurando los siguientes parámetros:
                 :param n_components: int
-                    The number of clusters in which the algorithm must split
-                    the data set
+                    El número de clusters en los que el algoritmo debe dividir el conjunto de datos
                 :param max_iter: int, default = 100
-                    The number of iteration that the algorithm will go throw to find the clusters
-                :param comp_names: list of strings, default=None
-                    In case it is setted as a list of string it will use to
-                    name the clusters
+                    El número de iteraciones que el algoritmo realizará para encontrar los clusters
+                :param comp_names: lista de strings, default=None
+                    En caso de que se establezca como una lista de cadenas se utilizará para
+                    nombrar los clusters.
         '''
         self.n_componets = n_components
         self.max_iter = max_iter
         if comp_names == None:
-            self.comp_names = [f"comp{index}" for index in range(self.n_componets)]
+            self.comp_names = [
+                f"comp{index}" for index in range(self.n_componets)]
         else:
             self.comp_names = comp_names
-        # pi list contains the fraction of the dataset for every cluster
+        # pi lista contiene la fracción del conjunto de datos para cada cluster
         self.pi = [1/self.n_componets for comp in range(self.n_componets)]
 
     def multivariate_normal(self, X, mean_vector, covariance_matrix):
         '''
-            This function implements the multivariat normal derivation formula,
-            the normal distribution for vectors it requires the following parameters
-                :param X: 1-d numpy array
-                    The row-vector for which we want to calculate the distribution
-                :param mean_vector: 1-d numpy array
-                    The row-vector that contains the means for each column
-                :param covariance_matrix: 2-d numpy array (matrix)
-                    The 2-d matrix that contain the covariances for the features
+            Esta función implementa la fórmula de derivación normal multivariante,
+            la distribución normal para vectores requiere los siguientes parámetros:
+                :param X: numpy array de 1 dimensión
+                    El vector fila para el cual deseamos calcular la distribución.
+                :param mean_vector: numpy array de 1 dimensión
+                    El vector fila que contiene las medias para cada columna.
+                :param covariance_matrix: numpy array de 2 dimensiones (matriz)
+                    La matriz 2-d que contiene las covarianzas de las características.
         '''
         return (2*np.pi)**(-len(X)/2)*np.linalg.det(covariance_matrix)**(-1/2)*np.exp(-np.dot(np.dot((X-mean_vector).T, np.linalg.inv(covariance_matrix)), (X-mean_vector))/2)
 
     def fit(self, X):
         '''
-            The function for training the model
-                :param X: 2-d numpy array
-                    The data must be passed to the algorithm as 2-d array, 
-                    where columns are the features and the rows are the samples
+            Esta función es utilizada para entrenar el modelo.
+                :param X: numpy array de 2 dimensiones
+                    Los datos deben pasarse al algoritmo como una matriz 2-d,
+                    donde las columnas son las características y las filas son las muestras.
         '''
-        # Spliting the data in n_componets sub-sets
+        # División de los datos en n_componets subconjuntos
         new_X = np.array_split(X, self.n_componets)
-        # Initial computation of the mean-vector and covarience matrix
+        # Cálculo inicial del vector medio y la matriz de covarianza
         self.mean_vector = [np.mean(x, axis=0) for x in new_X]
         self.covariance_matrixes = [np.cov(x.T) for x in new_X]
-        # Deleting the new_X matrix because we will not need it anymore
+        # Eliminando la matriz new_X porque no la necesitaremos más
         del new_X
         for iteration in range(self.max_iter):
             ''' --------------------------   E - STEP   -------------------------- '''
-            # Initiating the r matrix, evrey row contains the probabilities
-            # for every cluster for this row
+            # Iniciando la matriz r, cada fila contiene las probabilidades
+            # para cada cluster para esta fila.
             self.r = np.zeros((len(X), self.n_componets))
-            # Calculating the r matrix
+            # Calculando la matriz r
             for n in range(len(X)):
                 for k in range(self.n_componets):
-                    self.r[n][k] = self.pi[k] * self.multivariate_normal(X[n], self.mean_vector[k], self.covariance_matrixes[k])
-                    self.r[n][k] /= sum([self.pi[j]*self.multivariate_normal(X[n], self.mean_vector[j], self.covariance_matrixes[j]) for j in range(self.n_componets)])
-            # Calculating the N
+                    self.r[n][k] = self.pi[k] * self.multivariate_normal(
+                        X[n], self.mean_vector[k], self.covariance_matrixes[k])
+                    self.r[n][k] /= sum([self.pi[j]*self.multivariate_normal(
+                        X[n], self.mean_vector[j], self.covariance_matrixes[j]) for j in range(self.n_componets)])
+            # Se calcula N
             N = np.sum(self.r, axis=0)
             ''' --------------------------   M - STEP   -------------------------- '''
-            # Initializing the mean vector as a zero vector
+            # Inicializar el vector de medias como un vector de ceros
             self.mean_vector = np.zeros((self.n_componets, len(X[0])))
-            # Updating the mean vector
+            # Actualizar el vector de medias
             for k in range(self.n_componets):
                 for n in range(len(X)):
                     self.mean_vector[k] += self.r[n][k] * X[n]
-            self.mean_vector = [1/N[k]*self.mean_vector[k] for k in range(self.n_componets)]
-            # Initiating the list of the covariance matrixes
-            self.covariance_matrixes = [np.zeros((len(X[0]), len(X[0]))) for k in range(self.n_componets)]
-            # Updating the covariance matrices
+            self.mean_vector = [1/N[k]*self.mean_vector[k]
+                                for k in range(self.n_componets)]
+            # Inicializar la lista de matrices de covarianza
+            self.covariance_matrixes = [
+                np.zeros((len(X[0]), len(X[0]))) for k in range(self.n_componets)]
+            # Actualizar las matrices de covarianza
             for k in range(self.n_componets):
-                self.covariance_matrixes[k] = np.cov(X.T, aweights=(self.r[:, k]), ddof=0)
-            self.covariance_matrixes = [1/N[k]*self.covariance_matrixes[k] for k in range(self.n_componets)]
-            # Updating the pi list
+                self.covariance_matrixes[k] = np.cov(
+                    X.T, aweights=(self.r[:, k]), ddof=0)
+            self.covariance_matrixes = [
+                1/N[k]*self.covariance_matrixes[k] for k in range(self.n_componets)]
+            # Actualizar la lista pi
             self.pi = [N[k]/len(X) for k in range(self.n_componets)]
+
     def predict(self, X):
         '''
-            The predicting function
-                :param X: 2-d array numpy array
-                    The data on which we must predict the clusters
+            Función para predecir la pertenencia a los clusters.
+                :param X: numpy array 2D
+                    Los datos en los que debemos predecir los clusters.
         '''
-        probas = []
+        # Inicializamos la matriz de probabilidades
+        probabilities = []
+        # Calculamos las probabilidades para cada cluster
         for n in range(len(X)):
-            probas.append([self.multivariate_normal(X[n], self.mean_vector[k], self.covariance_matrixes[k])
-                           for k in range(self.n_componets)])
+            probabilities.append([self.multivariate_normal(X[n], self.mean_vector[k], self.covariance_matrixes[k])
+                                  for k in range(self.n_componets)])
         cluster = []
-        for proba in probas:
-            cluster.append(self.comp_names[proba.index(max(proba))])
+        # Devolvemos los nombres de los clusters correspondientes a los índices
+        for probabilitie in probabilities:
+            cluster.append(probabilitie.index(max(probabilitie)) + 1)
         return cluster
